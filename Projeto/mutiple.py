41eb6a7b716f9c49b6571e29b4f042d4e0773e55
Maria Laura Soares <mlaura@tecgraf.puc-rio.br>
2024-03-18 18:13:42 -0300

    mutiple done

--- Projeto/mutiple.py
+++ Projeto/mutiple.py
@@ -1,11 +1,12 @@
 #Métodos para leitura do Mutiple Welllog
-import numpy as np
+import time as tm
+import pandas as pd
 
 def read(path):
     file = open(path, "r")
     return file
 
-def countProperties(path): # retorna a quantidade de propriedade em um arquivo
+def getWellPropCount(path): # retorna a quantidade de propriedade em um arquivo
     # como as colunas sempre aparecem na primeira linha, pegamos direto ela
     file = read(path)
     linha = file.readline().rstrip() + " " #tira o \n e adiciona um espaço ao final
@@ -22,7 +23,7 @@ def countProperties(path): # retorna a quantidade de propriedade em um arquivo
             aux +=1
     return coluna
     
-def getProperties(path): # retorna uma lista com o nome das propriedades
+def getPropNames(path): # retorna uma lista com o nome das propriedades
     file = read(path)
     linha = file.readline().rstrip() + " " #tira o \n e adiciona um espaço ao final
     properties = []
@@ -59,26 +60,22 @@ def onlyNumbers(row):
         else:
             return True
 
-def empty(row):
+def empty(row): #verifica de uma linha é vazia
     letras = -1
     numeros = -1
     for i in range(len(row)):
         if(ord(row[i]) >= 48 and  ord(row[i]) <= 57): # verfica se é um número
             numeros = 0
-            print("achei um numero")
         if(ord(row[i]) >= 65 and  ord(row[i]) <= 90): # verfica se é uma letra
             letras = 0
-            print("achei uma letra")
         elif(ord(row[i]) >= 97 and  ord(row[i]) <= 122):
             letras = 0
-            print("achei uma letra")
     if(letras == 0 or numeros == 0):
         return False
     else:
         return True
-
         
-def getNameDateWell(row):
+def getNameDateWell(row): # retorna o nome e a data de um poço
     # - the well name must be enclosed in single or double quotes
 
     aux = 0 # contador de aspas
@@ -86,7 +83,7 @@ def getNameDateWell(row):
     name = ""
     date = "" # formato a data?
     dados = []
-    print(f"Essa é a row {row}")
+    
     for i in range(len(row)):
         if(row[i] in aspas):
             if(aux != 2):
@@ -102,16 +99,14 @@ def getNameDateWell(row):
     dados.append(date)
     return dados
 
-def getWellNames(path):
+def getWellNames(path): # retorna uma lista com os nomes dos poços de um arquivo
     """
     info:
     - the well name must be enclosed in single or double quotes
     - logunits are optional
     - logunits when are included on the file, they are located at the second line
     """
-
     file = read(path)
-    #linha = file.readline().rstrip()
     logunits = -1 # variavel auxiliar para identificar se já a leitura dos dados dos poços já se inciou
     unidades = "logunits"
     cont  = 1 # contador de linhas
@@ -131,95 +126,121 @@ def getWellNames(path):
                 well_names.append(poco_nome[0])  
                 logunits = -1 # já passou pelo logunits            
             elif(not empty(linha)): # se a linha não foi vazia
-                #lista = linha.split(" ")
                 if(not onlyNumbers(linha)): # se a linha não tiver apenas números
-                    poco_nome = getNameDateWell(linha)  
-                    print(poco_nome)
+                    poco_nome = getNameDateWell(linha)
                     well_names.append(poco_nome[0])   
         cont += 1
     return well_names
-    
 
-"""def organize(path, columns): # organiza os dados de cada poço em un dicionario
+def getWellCount_v1(path):
+    wells = getWellNames(path)
+    countWells = len(wells)
+    return countWells
 
+def getWellCount(path): # retorna a quantidade de poços de um arquivo
     file = read(path)
-    l=[]                            #lista onde vão ficar todos valores de cada coluna 
-    dWlg = dict()                   
-    nomeEdata = []                  #lista que pega os nomes e as datas
-    num = 0                         #contagem de valores que tem em um unico poço
-    qtd = []                        #lista com as contagens acima
+    logunits = -1 # variavel auxiliar para identificar se já a leitura dos dados dos poços já se inciou
+    unidades = "logunits"
+    cont  = 1 # contador de linhas
+    countWells = 0
 
     for linha in file:
-        linha = linha.strip().split()
-        if linha != []:             #verifica se tem espaços vazios dentro do arquivo
-            if linha[0][0] == '"' or linha[0][0] == "'":   #verifica se a linha é o nome e a data
-                nomeEdata.append(linha)
-                qtd.append(num)
-                num = 0
-            else:
-                if len(linha) == colunas:                     #verifica se a quantidade de indices é igual as colunas
-                    l.append(linha)
-                num += 1
-    qtd.pop(0)                                        #remove o numero 0
-    qtd.append(num)
+        if(cont == 2):
+            row = linha.lower()
+            if(unidades in row): # verifica se a linha 2 é logunits (opcional nos documentos)
+                logunits = 0 # atribui valor zero caso seja logunits
+        else:
+            if(cont > 2):
+                if(logunits == 0):
+                    logunits = -1
+                    countWells +=1
+                else:
+                    if(not empty(linha) and not onlyNumbers(linha)):
+                        countWells += 1
+        cont +=1
+    return countWells
 
-
-    j = 0
-    for n, item in enumerate(qtd):                       #começa a organizar as listas
-        divisor = []                                     
-        for index in range(item):
-            divisor.append(l[j])                      
-            j += 1
-        nome = nomeEdata[n][0] + "__" + nomeEdata[n][1]  #separa o nome e a data com um delimitador e adiciona
-        dWlg[nome] = divisor                             #na chave do dicionario
-
-
-
-    for k, v in dWlg.items():                            #for só pra printar o dicionario
-        print(k, v)
-        print()"""
-
-
-
-
-#arq = "arquivos\RFT_mod_netto.wlg"
-
-#colu = countProperties(arq)
-#getProperties(arq)
-#organiza(arq, colu)
-#print(getNameDateWell("'RCB-0001' 19820407"))
-#print(readingData("1926.0 159.9"))
-print(getWellNames("arquivos/multiplewelllogwith2d.wlg"))
-#print(empty("\t\n alvcmvbh9087645"))
-#print(onlyNumbers("\t\n86947746752"))
-
-
-
-
-
-# ----------------------------------------------------------IGNORAR----------------------------------------------------------
-
-    # x = 0
-    # b = 0
-    # for j in range(cont):
-    #     a = []
-    #     for index, itens in enumerate(l):
-    #         if index == x:
-    #             nome = nomeEdata[0][0]
-    #             a.append(nome)
-    #             x = x + qtd[b]
-    #             b += 1
-    #         a.append(itens[j])
-
-        
-    #     dWlg[j] = a
+def replace(row, a, b):
+    space = [a, b]
+    dados = list(row)
+    for i in range(len(dados)):
+        if(dados[i] in space):
+            dados[i] = " "
+    return "".join(dados)
     
+def organizeData(lista, colunas, n_cols): # organiza os dados de acordo com as propriedades do documento
+    # lista são os dados de um determindado well
+    # colunas são as proriedades listadas no arquivo
+    # n_cols é o número de propriedades listadas no arquivo
+    data = dict()
+    j = 0
+    while(j < n_cols): # para cada coluna
+        key = colunas[j] # atribui key do dicionario o nome da coluna
+        valor = [] # cria uma lista para reorganizar os dados de acordo com sua coluna
+        for item in lista: # percorre a lista (cada item é uma linha)
+            valor.append(item[j]) #pega o valor da linha respectivo a coluna
+            data[key] = valor # atribui os dados organizados a sua respectiva coluna
+        j +=1
+    return data
 
+def getWellPropData(path):
+    colunas = getPropNames(path)
+    n_cols = getWellPropCount(path)
+    well_names = getWellNames(path)
+    totalWells = getWellCount(path)
+    file = read(path)
+    aux = 0
+    i = 0 # contador de pocos
+    data_per_well = dict()
+    well = ""
+    dados = []
+    wellPropData = []
 
-    # myvar = pd.DataFrame(dWlg)
-    # print(myvar)
+    for linha in file:
+        linha = linha.rstrip()
+        if(well_names[i] in linha):
+            if(aux > 0):
+                data_per_well[well] = dados
+                dados = []
+                well = well_names[i]
+                if(i < totalWells-1):
+                    aux += 1
+                    i += 1        
+            else:
+                well = well_names[i]
+                aux += 1
+                i += 1
+        elif(aux <= totalWells):
+            if(not empty(linha) and onlyNumbers(linha)): # verificando se estamos na linha que contém dados
+                linha = replace(linha,  "\n", "\t")
+                dados.append(linha.split())
+        if(i == totalWells - 1):
+            data_per_well[well] = dados
+  
+    for key in data_per_well: # reorganiza os dados de cada well de acordo com suas propriedades
+        dados = data_per_well.get(key)
+        data_per_well[key] = organizeData(dados, colunas, n_cols)
 
-    # for k, v in dWlg.items():
-    #     print(k)
-    #     myvar = pd.DataFrame(dWlg[k])
-    #     print(myvar)
+    for i in range(totalWells):
+        key = well_names[i]
+        table = pd.DataFrame(data_per_well.get(key))
+        wellPropData.append(table)
+
+    return wellPropData
+
+# wellproptype - TO DO    
+
+"""
+- algoritmo de tempo de execucao 
+start_time = tm.time()
+lista = getWellPropData("arquivos/RFT_LINGUADO.wlg")
+well = getWellNames("arquivos/RFT_LINGUADO.wlg")
+for i in range(len(lista)):
+        print("\n")
+        print(well[i])
+        print(lista[i])
+end_time = tm.time()
+execution_time = end_time-start_time
+
+print(f"\n\nExecution Time: {execution_time}")
+"""
--- Projeto/single.py
+++ Projeto/single.py
@@ -1,31 +1,27 @@
-#Métodos para leitura do Single e Mutiple Welllog
+#Métodos para leitura do Single Welllog
 
 def read(path):
     file = open(path, "r")
     return file
 
-def countProperties(path): #DONE
-    # como as colunas sempre aparecem na primeira linha, pegamos direto ela
-    file = read(path)
-    linha = file.readline().rstrip() + " " #tira o \n e adiciona um espaço ao final
-    coluna = 0
-    aux = 0 # conta as aspas
-    space = [" ", "\t"]
-    for i in range(len(linha)):
-        if(linha[i] in space and linha[i-1] != " "): #verifica se já passamos por uma coluna (estamos entre colunas, ou seja, em um "espaço")
-            if(aux == 0 or aux == 2): # se acabou de ler uma coluna com ou sem aspas
-                coluna +=1
-        elif(linha[i] == "'"): # começa a ler uma coluna entre aspas
-            if(aux == 2): # se leu a última aspa
-                aux = 0
-            aux +=1
-    return coluna
-    
-def getProperties(path): # ajeitar no formato de cima --> mesma coisa mas com o col_name
-    file = read(path)
-    linha = file.readline().rstrip() + " " #tira o \n e adiciona um espaço ao final
+def empty(row): #verifica de uma linha é vazia
+    letras = -1
+    numeros = -1
+    for i in range(len(row)):
+        if(ord(row[i]) >= 48 and  ord(row[i]) <= 57): # verfica se é um número
+            numeros = 0
+        if(ord(row[i]) >= 65 and  ord(row[i]) <= 90): # verfica se é uma letra
+            letras = 0
+        elif(ord(row[i]) >= 97 and  ord(row[i]) <= 122):
+            letras = 0
+    if(letras == 0 or numeros == 0):
+        return False
+    else:
+        return True
+
+def getColumns(linha): # identifica e retorna o nome das colunas
     properties = []
-    aux = 0
+    aux = 0 # conta as aspas
     col = ""
     space = [" ", "\t"]
     for i in range(len(linha)):
@@ -43,9 +39,35 @@ def getProperties(path): # ajeitar no formato de cima --> mesma coisa mas com o 
             col += linha[i]
     return properties
 
-
-#print(countProperties("Projeto/modelo.wlg"))
-#print(getProperties("Projeto/modelo.wlg"))
+def getPropNames(path):
+    # TThe first line is the well name and an optional date and/or null value. 
+    # The next line may optionally contain log units which must be input using the keyword LOGUNITS
+    # The next line contains the log names (column headings). 
     
+    file = read(path)
+    cont = 1 # contador de linhas
+    logunits = -1 # variavel auxiliar para identificar se já a leitura dos dados dos poços já se inciou
+    unidades = "logunits"
+    propNames = []
 
+    for linha in file:
+        linha = linha.rstrip() + " " #tira o \n e adiciona um espaço ao final
+        if(cont == 2 and not empty(linha)):
+            row = linha.lower()
+            if(unidades not in row): # verifica se a linha 2 é logunits (opcional nos documentos)
+                propNames = getColumns(linha)
+                break
+        elif(cont > 2):
+            if(not empty(linha)):
+                propNames = getColumns(linha)
+                break
+        cont +=1
+    
+    return propNames
 
+def getWellPropCount(path):
+    propNames = getPropNames(path)
+    return len(propNames)
+    
+print(getPropNames("arquivos/2_singlewelllogwithnullvalue.log"))
+print(getWellPropCount("arquivos/2_singlewelllogwithnullvalue.log"))
